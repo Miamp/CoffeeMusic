70c94bdf5f60cd4b58a75a101ddc8f262aab87f7
Mildred <millymartz23@gmai.com>
2022-11-17 18:42:16 -0600

    color modified

--- src/components/layouts/App.jsx
+++ src/components/layouts/App.jsx
@@ -3,11 +3,11 @@ export default function App () {
     <div className='container-fluid bg-main bg-p'>
       <div className='container '>
         <div className='row justify-content-end align-items-end'>
-          q
+        <h1 className='title-gradient'>CoffeeMusic</h1>
         </div>
         <div className='row justify-content-center align-items-center vh-100 '>
           <div className='col-7'>
-            <h1 className='title-gradient'>CoffeeMusic</h1>
+            
           </div>
           <div className='col-3 bg-s'>
             <input type='search' name='' id='' />
