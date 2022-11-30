03d90dc630f85f19ffef007c620fbe39c81f0a79
Mildred <millymartz23@gmai.com>
2022-11-29 20:20:54 -0600

    base

--- src/components/layouts/App.jsx
+++ src/components/layouts/App.jsx
@@ -1,16 +1,24 @@
 export default function App () {
   return (
-    <div className='container-fluid bg-main bg-p'>
-      <div className='container '>
-        <div className='row justify-content-end align-items-end'>
-        <h1 className='title-gradient'>CoffeeMusic</h1>
+    <div className='container-fluid bg-main '>
+      <div className='container'>
+        <div className='row'>
+          <h1 className='text-center title-gradient'>CoffeeMusic</h1>
         </div>
-        <div className='row justify-content-center align-items-center vh-100 '>
-          <div className='col-7'>
-            
+        <div className='row justify-content-center align-items-center base-container mt-5'>
+          <div className='col-3 bg-m-opacity'>
+            <h5>Columna de menu</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis quisquam officiis labore, rerum molestiae sed quia assumenda magni aspernatur facere ipsum voluptatem cumque similique. Non molestiae velit ex magnam et.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea repudiandae corporis qui numquam ab accusamus sequi rem tempora quas molestias, nobis hic nulla exercitationem ad quo animi beatae obcaecati maiores!
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit sint in aperiam maiores, placeat dignissimos commodi iure praesentium illo fugiat nihil ab adipisci, blanditiis optio temporibus ipsa? Reprehenderit, est illum!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus error omnis nobis quod itaque nisi qui, rem vero facere quia neque fugiat, ea recusandae expedita nesciunt quis laudantium, impedit repellendus?
+            </p>
           </div>
-          <div className='col-3 bg-s'>
-            <input type='search' name='' id='' />
+          <div className='col-8 bg-c-opacity'>
+            <h5>Columna de contenido</h5>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis, magnam ipsum pariatur ullam sequi id perspiciatis provident quasi exercitationem deserunt laboriosam sapiente, sunt, minus iste officiis dolores iure tempore tenetur!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed accusantium accusamus delectus! Aperiam earum nobis quia maiores sit incidunt iure ad deserunt. Nobis incidunt repellat rem veniam quam adipisci placeat.
+            </p>
           </div>
         </div>
       </div>
